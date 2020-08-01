5508b90795b109dc7787a270929738c73cbc0046
Andrew Musali <andrewmusali@gmail.com>
2020-08-01 05:16:22 +0300

    creates password generator

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	mode:'development',
+	entry: './src/index.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use:{
+					loader: 'babel-loader',
+					options: {
+						presets:['@babel/preset-env']
+					}
+				}
+			}
+		]
+	}
+}
